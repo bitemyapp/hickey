2772097b18cfc73518217c86b04a02451686c160
Michael Walker <mike@barrucadu.co.uk>
2014-08-01 13:33:08 +0100

    Start to flesh out Store

--- src/Store.hs
+++ src/Store.hs
@@ -15,12 +15,16 @@ module Store
     , getDiff
     ) where
 
-import Control.Monad.IO.Class (MonadIO)
-import Data.Text (Text)
+import Control.Applicative ((<$>))
+import Control.Monad.IO.Class (liftIO)
 import Data.ByteString.Lazy (ByteString)
-import Types
 import Data.FileStore.Generic (Diff)
-import Data.FileStore.Types (RevisionId, UTCTime)
+import Data.FileStore.Git (gitFileStore)
+import Data.FileStore.Types
+import Data.Text (Text, pack, unpack)
+import Routes
+import Types
+import Web.Seacat (RequestProcessor, conf')
 
 data Commit = Commit { commitRevision :: Revision
                      , commitUnderlyingRevision :: RevisionId
@@ -31,30 +35,43 @@ data Commit = Commit { commitRevision :: Revision
 
 type Differences = [Diff [Text]]
 
+instance Contents Text where
+    fromByteString = pack . fromByteString
+    toByteString   = toByteString . unpack
+
+-- |Run a function that takes a reference to the filestore.
+withFileStore :: (FileStore -> IO a) -> RequestProcessor Sitemap a
+withFileStore f = do
+  git <- gitFileStore <$> conf' "git" "path"
+  liftIO $ initialize git
+  liftIO $ f git
+
+-----
+
 -- |Get the contents of the HEAD of a text file, if it exists.
-getStoredFile :: MonadIO m => FileName -> m (Maybe Text)
-getStoredFile fn = undefined
+getStoredFile :: FileName -> RequestProcessor Sitemap (Maybe Text)
+getStoredFile fn = withFileStore undefined
 
 -- |Get the contents of a specific revision of a text file. If the
 -- file didn't exist at that revision, return Nothing.
-getStoredFileAt :: MonadIO m => FileName -> Revision -> m (Maybe Text)
-getStoredFileAt fn r = undefined
+getStoredFileAt ::  FileName -> Revision -> RequestProcessor Sitemap (Maybe Text)
+getStoredFileAt fn r = withFileStore undefined
 
 -- |Get the contents of the HEAD of a binary file, if it exists, as a
 -- lazy bytestring.
-getStoredBinary :: MonadIO m => FileName -> m (Maybe ByteString)
-getStoredBinary fn = undefined
+getStoredBinary ::  FileName -> RequestProcessor Sitemap (Maybe ByteString)
+getStoredBinary fn = withFileStore undefined
 
 -- |Get the contents of a specific revision of a binary file, if it
 -- exists, as a lazy bytestring.
-getStoredBinaryAt :: MonadIO m => FileName -> Revision -> m (Maybe ByteString)
-getStoredBinaryAt fn r = undefined
+getStoredBinaryAt ::  FileName -> Revision -> RequestProcessor Sitemap (Maybe ByteString)
+getStoredBinaryAt fn r = withFileStore undefined
 
 -- |Get the history of a file, if it exists, as a list of commits.
-getHistory :: MonadIO m => FileName -> m (Maybe [Commit])
-getHistory fn = undefined
+getHistory ::  FileName -> RequestProcessor Sitemap (Maybe [Commit])
+getHistory fn = withFileStore undefined
 
 -- |Get the diff of a file between two revisions, if they're good and
 -- the file exists, as a list of changes.
-getDiff :: MonadIO m => FileName -> Revision -> Revision -> m (Maybe Differences)
-getDiff fn r1 r2 = undefined
+getDiff ::  FileName -> Revision -> Revision -> RequestProcessor Sitemap (Maybe Differences)
+getDiff fn r1 r2 = withFileStore undefined
