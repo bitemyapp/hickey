d46e95ff8ba2548f1310dce6896bbd147d145a8f
Michael Walker <mike@barrucadu.co.uk>
2014-08-02 23:27:08 +0100

    Add some template utils for forms and Text

--- src/Templates/Utils.hs
+++ src/Templates/Utils.hs
@@ -1,13 +1,23 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Templates.Utils
     ( link
-    , link') where
+    , link'
+
+    , input
+    , input'
+
+    , toHtml
+    , empty) where
 
 import Data.Text (Text)
 import Routes (Sitemap)
-import Text.Blaze.Html5 (Html, (!), a, toHtml)
-import Text.Blaze.Html5.Attributes (href, title)
+import Text.Blaze.Html5 (Html, (!), a, label)
+import Text.Blaze.Html5.Attributes (for, href, name, required, title, type_)
 import Text.Blaze.Internal (textValue)
 import Web.Seacat (MkUrl)
+
+import qualified Text.Blaze.Html5 as H
 
 -- |Render a link to HTML, where the inner text is the same as the
 -- title.
@@ -32,3 +42,36 @@ link' :: MkUrl Sitemap
       -- ^The target
       -> Html
 link' mkurl ttl text target = a ! href (textValue $ mkurl target []) ! title (textValue ttl) $ toHtml text
+
+-- |Render an optional input element with a label.
+input :: Text
+      -- ^The label text
+      -> Text
+      -- ^The field name
+      -> Text
+      -- ^The field type
+      -> Html
+input lbl nam typ = do
+  label ! for (textValue nam) $ toHtml lbl
+  H.input ! name (textValue nam) ! type_ (textValue typ)
+
+-- |Render a required input element with a label.
+input' :: Text
+       -- ^The label text
+       -> Text
+       -- ^The field name
+       -> Text
+       -- ^The field type
+       -> Html
+input' lbl nam typ = do
+  label ! for (textValue nam) $ toHtml lbl
+  H.input ! name (textValue nam) ! type_ (textValue typ) ! required (textValue "required")
+
+-- |A specialised toHtml, to get around the issues caused by
+-- OverloadedStrings.
+toHtml :: Text -> Html
+toHtml = H.toHtml
+
+-- |Some empty HTML
+empty :: Html
+empty = toHtml ""
