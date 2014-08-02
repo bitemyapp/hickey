004c7c7f7c7415659474d29fa0667016e513db5a
Michael Walker <mike@barrucadu.co.uk>
2014-08-02 19:08:01 +0100

    Implement retrieving files from the store

--- src/Store.hs
+++ src/Store.hs
@@ -16,11 +16,12 @@ module Store
     ) where
 
 import Control.Applicative ((<$>))
+import Control.Exception (catch)
 import Control.Monad.IO.Class (liftIO)
 import Data.ByteString.Lazy (ByteString)
 import Data.FileStore.Generic (Diff)
 import Data.FileStore.Git (gitFileStore)
-import Data.FileStore.Types
+import Data.FileStore.Types (Contents, FileStore(..), FileStoreError, RevisionId, UTCTime, toByteString, fromByteString)
 import Data.Text (Text, pack, unpack)
 import Routes
 import Types
@@ -43,19 +44,37 @@ instance Contents Text where
 withFileStore :: (FileStore -> IO a) -> RequestProcessor Sitemap a
 withFileStore f = do
   git <- gitFileStore <$> conf' "git" "path"
-  liftIO $ initialize git
   liftIO $ f git
+
+-- |Convert a FileName to a FilePath, relative to the root of the file
+-- store.
+filePath :: FileName -> FilePath
+filePath = unpack . fileTextName
+
+-- |Catch a FileStore exception
+catchStoreExc :: IO a -> (FileStoreError -> IO a) -> IO a
+catchStoreExc = catch
+
+-- |Return the second argument if a FileStore exception is caught.
+onStoreExc :: IO a -> IO a -> IO a
+onStoreExc dangerous = catchStoreExc dangerous . const
 
 -----
 
 -- |Get the contents of the HEAD of a text file, if it exists.
 getStoredFile :: FileName -> RequestProcessor Sitemap (Maybe Text)
-getStoredFile fn = withFileStore undefined
+getStoredFile fn = getStoredFileAt' (filePath fn) Nothing
 
 -- |Get the contents of a specific revision of a text file. If the
 -- file didn't exist at that revision, return Nothing.
-getStoredFileAt ::  FileName -> Revision -> RequestProcessor Sitemap (Maybe Text)
-getStoredFileAt fn r = withFileStore undefined
+getStoredFileAt :: FileName -> Revision -> RequestProcessor Sitemap (Maybe Text)
+getStoredFileAt fn r = getStoredFileAt' (filePath fn) $ Just r
+
+-- |Get the contents of a file at the given revision, or the latest if
+-- the revision is unspecified.
+getStoredFileAt' :: FilePath -> Maybe Revision -> RequestProcessor Sitemap (Maybe Text)
+getStoredFileAt' fn r = withFileStore $ \fs -> contents fs `onStoreExc` return Nothing
+    where contents fs = Just <$> retrieve fs fn Nothing
 
 -- |Get the contents of the HEAD of a binary file, if it exists, as a
 -- lazy bytestring.
