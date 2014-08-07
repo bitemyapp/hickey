4d79367ebbcbb0f04444eb8adcbcd180ed9a6019
Michael Walker <mike@barrucadu.co.uk>
2014-08-07 13:00:50 +0100

    Tidy up Types

--- src/Types.hs
+++ src/Types.hs
@@ -1,44 +1,15 @@
-{-# LANGUAGE OverloadedStrings #-}
-
-module Types
-    ( WikiPage
-    , pageTextName
-    , isPageName
-    , toWikiPage
-
-    , Revision
-    , revisionTextId
-    , revisionShortId
-    , isRevisionId
-    , toRevision
-
-    , FileName
-    , fileTextName
-    , isFileName
-    , toFileName
-
-    , Plugin
-    ) where
+module Types where
 
 import Data.Char (isAlphaNum, isHexDigit)
-import Data.Text (Text, splitOn)
+import Data.Text (Text)
 
 import qualified Data.Text as T
+
+-- *Wiki pages
 
 -- |A wiki page name is some sequence of alphanumeric characters.
 newtype WikiPage = WikiPage { pageTextName :: Text }
     deriving (Eq, Show)
-
--- |A revision identifier is a hex string.
-newtype Revision = Revision { revisionTextId :: Text }
-    deriving (Eq, Show)
-
--- |A filename is a string [a-zA-Z0-9_-] with a single '.'.
-newtype FileName = FileName { fileTextName :: Text }
-    deriving (Eq, Show)
-
--- |A plugin has a name, and a path to an executable.
-type Plugin = (String, FilePath)
 
 -- |Check if some text is a valid page name
 isPageName :: Text -> Bool
@@ -48,6 +19,12 @@ isPageName = T.all isAlphaNum
 toWikiPage :: Text -> Maybe WikiPage
 toWikiPage p | isPageName p = Just $ WikiPage p
              | otherwise    = Nothing
+
+-- *Revisions
+
+-- |A revision identifier is a hex string.
+newtype Revision = Revision { revisionTextId :: Text }
+    deriving (Eq, Show)
 
 -- |Get an abbreviated version of a revision ID.
 revisionShortId :: Revision -> Text
@@ -62,15 +39,26 @@ toRevision :: Text -> Maybe Revision
 toRevision r | isRevisionId r = Just $ Revision r
              | otherwise      = Nothing
 
+-- *Files
+
+-- |A filename is a string [a-zA-Z0-9_-] with a single '.'.
+newtype FileName = FileName { fileTextName :: Text }
+    deriving (Eq, Show)
+
 -- |Check if some text is a valid filename.
 isFileName :: Text -> Bool
 isFileName fn = plen && palpha
     where palpha = all (T.all isFileChr) parts
           plen = length parts == 2
-          parts = splitOn "." fn
+          parts = T.split (=='.') fn
           isFileChr c = isAlphaNum c || c == '-' || c == '_'
 
 -- |Convert some text to a filename, if valid.
 toFileName :: Text -> Maybe FileName
 toFileName f | isFileName f = Just $ FileName f
              | otherwise    = Nothing
+
+-- *Misc
+
+-- |A plugin has a name, and a path to an executable.
+type Plugin = (String, FilePath)
